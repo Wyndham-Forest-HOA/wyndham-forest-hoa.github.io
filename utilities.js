d0805403460ac39794fb1d083c889ada406765fe
Lakshmi Dayalan <lakshmidayalan@LaLA-MacBook-Air.local>
2020-04-06 07:17:53 -0400

    Updated functions

--- utilities.js
+++ utilities.js
@@ -1,16 +1,39 @@
-<script>
-function myFunction() {
-  var copyText = document.getElementById("myInput");
-  copyText.select();
-  copyText.setSelectionRange(0, 99999);
-  document.execCommand("copy");
-  
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copied: " + copyText.value;
+function showCopyIcon(elementId) {
+    divId=elementId.id
+    copySpanId=(elementId.id+"CopySpan")
+    var copySpan = document.getElementById(copySpanId);
+    copySpan.hidden=false;
 }
 
-function outFunc() {
-  var tooltip = document.getElementById("myTooltip");
-  tooltip.innerHTML = "Copy to clipboard";
+function hideCopyIcon(elementId) {
+    divId=elementId.id
+    copySpanId=(elementId.id+"CopySpan")
+    var copySpan = document.getElementById(copySpanId);
+    copySpan.hidden=true;
+ }
+
+function copyLink(divId) {
+    try {
+        hrefId=divId+"Href"
+        linkURL = document.getElementById(hrefId).href;
+
+        var textArea = document.createElement("textarea");
+        textArea.value = linkURL;
+        // Avoid scrolling to bottom
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.position = "fixed";
+    
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+    
+
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('copyLink: Copying text command was ' + msg);
+        document.body.removeChild(textArea);
+    }catch (err) {
+        console.error('copyLink: Error Copying', err);
+      }
 }
-</script>
